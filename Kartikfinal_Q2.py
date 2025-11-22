726b6bcbea548baf6317a062f590269f2177e76d
sameerbhasin6-lgtm <sameerbhasin6@gmail.com>
2025-11-23 04:20:00 +0530

    Update Kartikfinal_Q2.py

--- Kartikfinal_Q2.py
+++ Kartikfinal_Q2.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from io import StringIO
-import plotly.graph_objects as go
 import plotly.express as px
 
 # --- PAGE CONFIGURATION ---
